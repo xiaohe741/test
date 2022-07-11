704309b155ba52e2d4a7daf47eaa01fdf18cfe83
xiaohe741 <xiaohe741@sina.com>
2022-07-11 11:05:00 +0800

    add images.js

---
+++ images.js
@@ -0,0 +1,47 @@
+/**
+ * +-------------------------------------------------
+ * |    Vendor / Image.js
+ * +-------------------------------------------------
+ * |    Charlie / xiaohe741@sina.com
+ * +-------------------------------------------------
+ */
+
+// export const avatar = [
+// 	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/0c300392-ee01-4a3a-8aa5-1f8ce65bc998.jpg',
+// 	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/52d7d301-aedd-45bb-adb9-6a3f0981196f.jpg',
+// 	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/40fac287-d159-493c-a29f-88125f6882f5.jpg',
+// 	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/b7341683-22f8-4887-b842-99ab1a7274d3.jpg',
+// 	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-aliyun-eq9pywhbwvmv3914cc/f21c4058-88f7-448a-8730-456fa68e83ef.jpg',
+// ];
+
+export const photos = [
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/102b2910-eea3-4ff0-bc39-eec4f80d0055.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/2c1ca319-8049-47c3-9784-79147c641e63.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/276aa471-74bd-49dc-9d46-aeb5cb0141ba.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/0f28a115-b413-4060-8a75-0f988a3d6458.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/3949f1c5-6d4a-44a1-a263-f8e95addbac5.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/136c5624-4d08-4a9a-8e68-4e9f1034aa44.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/696f2b38-2d1a-4629-a631-b5db03dd5c60.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/ac52ea35-eeb1-46bd-8c73-064a8bddb9f4.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/08b40f1c-ce14-4efd-a2d7-b2de2bfccb60.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/7b5a28b1-084b-446b-9bce-db1156ecba60.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/26fc898f-f183-474f-aca8-e8a9af754727.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/86b05ac4-8f9b-4c1a-8b21-d4141e49c8ef.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/da0c01fb-c54c-4443-8cae-61bf80f05b29.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/e3e2629f-6cb3-41f4-bec0-10c0b4960a00.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/e0dd6326-8a33-4dd5-954f-c64b7caa4b21.png',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/86983863-7839-4574-b77f-99a1994d94d1.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/fd710af7-44e5-4ea6-8a2a-73ed3c5c47cd.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/ee564df5-98c1-453b-90c5-524d4f7416a3.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/ae2c0ad6-1e48-4276-af49-37b3e823ee6f.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/236dc0c1-b3f6-4b91-9d82-b611f8b39239.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/9687bd0f-a18b-4283-9fe3-be19e9c9d3e9.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/ab660989-4bbd-4788-b374-b334a1e9eec2.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/97accb5f-0d07-463d-9edc-9f38dc1f35dd.jpg',
+	'https://vkceyugu.cdn.bspapp.com/VKCEYUGU-52a53d65-a6d2-4818-a34f-d2dd78d96bbb/bdfe262d-3afa-4e27-a344-75960acd4e68.jpg',
+]
+
+export default {
+	// avatar,
+	photos,
+}
